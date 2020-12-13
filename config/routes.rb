9c2b54e5913510a38e4186b03cacf5f7901a953e
madelinemc <madeline.coons@gmail.com>
2020-12-12 20:59:10 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  #get '/admin/stats', to: 'stats#index' instead:
+  scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
+  #or use rails namesapce method here as a shortcut to the above:
+  # namesapce :admin do
+  #   resources :stats, only: [:index]
+  # end
 
   root 'posts#index'
 end
